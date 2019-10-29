d1c6f086a44f738cc10ddcbacb4568dd71083d39
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2019-10-29 09:01:21 -0300

    Add Flag component

--- App.js
+++ App.js
@@ -42,6 +42,9 @@ class App extends Component {
                 <Field mined/>
                 <Field mined opened/>
                 <Field mined opened exploded/>
+                <Field flagged/>
+                <Field flagged opened/>
+
               </View>
             </View>
           </ScrollView>
