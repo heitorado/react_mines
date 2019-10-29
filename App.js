3c7d40a9c0ada03a89322a5fd6b59bb73fa8f0c4
Heitor Sammuel Carvalho Souza <heitor.scz@gmail.com>
2019-10-29 09:14:38 -0300

    Add bigger flag modifier

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   SafeAreaView,
@@ -13,13 +5,7 @@ import {
   ScrollView,
   View,
   Text,
-  StatusBar,
 } from 'react-native';
-
-import {
-  Header,
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 
 import params from './src/params'
 import Field from './src/components/Field'
@@ -28,69 +14,30 @@ class App extends Component {
   render() {
     return (
       <>
-        <SafeAreaView>
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={styles.scrollView}>
-            <View style={styles.body}>
-              <View style={styles.sectionContainer}>
-                <Text>Iniciando o Mines!</Text>
-                <Field/>
-                <Field opened/>
-                <Field opened nearMines={1}/>
-                <Field opened nearMines={2}/>
-                <Field mined/>
-                <Field mined opened/>
-                <Field mined opened exploded/>
-                <Field flagged/>
-                <Field flagged opened/>
-
-              </View>
-            </View>
-          </ScrollView>
-        </SafeAreaView>
+        <View style={styles.container}>
+          <Text>Iniciando o Mines!</Text>
+          <Field/>
+          <Field opened/>
+          <Field opened nearMines={1}/>
+          <Field opened nearMines={2}/>
+          <Field mined/>
+          <Field mined opened/>
+          <Field mined opened exploded/>
+          <Field flagged/>
+          <Field flagged opened/>
+        </View>
       </>
     )
   };
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
 });
 
 export default App;
